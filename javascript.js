17a2fc83f0b53eae33523141e8fb44d04a71793f
Anku2k05 <ankitadalai04@gmail.com>
2025-03-08 16:08:52 +0530

    updated css

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// let t1 =0;
+// let t2 = 1;
+// let t3= t1+t2;
+// console.log(t1);
+// console.log(t2);
+// for(let i=0; i<10; i++)
+//     {
+//         t3= t1+t2;
+//         console.log(t3  );
+//         t1=t2;
+//         t2=t3;
+//     }
+let fruits=["Apple","Banana", "Mango" , "Orange"];
+fruits.splice(2,1);
+console.log(fruits);
