2a03541975e82a7cc7bcad8bc58c2e114a024c67
Anku2k05 <ankitadalai04@gmail.com>
2025-03-08 22:21:28 +0530

    Added animation

--- javascript.js
+++ javascript.js
@@ -1,15 +1,38 @@
-// let t1 =0;
-// let t2 = 1;
-// let t3= t1+t2;
-// console.log(t1);
-// console.log(t2);
-// for(let i=0; i<10; i++)
-//     {
-//         t3= t1+t2;
-//         console.log(t3  );
-//         t1=t2;
-//         t2=t3;
-//     }
-let fruits=["Apple","Banana", "Mango" , "Orange"];
-fruits.splice(2,1);
-console.log(fruits);
+function changeAboutMeText() {
+    const aboutMeTexts = ["Tech Enthusiast", "Full Stack Web Developer"];
+    const typingSpeed = 100;
+    const erasingSpeed = 50;
+    const pauseTime = 1500;
+    const aboutMeElement = document.querySelector('.about-me');
+
+    let textIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    function type() {
+        const currentText = aboutMeTexts[textIndex];
+
+        if (!isDeleting && charIndex < currentText.length) {
+            // Typing
+            aboutMeElement.textContent = currentText.substring(0, charIndex + 1);
+            charIndex++;
+            setTimeout(type, typingSpeed);
+        } else if (isDeleting && charIndex > 0) {
+            // Erasing
+            aboutMeElement.textContent = currentText.substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(type, erasingSpeed);
+        } else {
+            // Pause before switching
+            isDeleting = !isDeleting;
+            if (!isDeleting) {
+                textIndex = (textIndex + 1) % aboutMeTexts.length;
+            }
+            setTimeout(type, pauseTime);
+        }
+    }
+
+    type();
+}
+
+changeAboutMeText();
